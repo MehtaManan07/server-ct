30ae90305e18512c1b9bc7859bb149fff53eef11
manan-schmooze <manan@schmoozedating.com>
2024-05-09 16:31:49 +0530

    typeorm config

---
+++ src/datasource/typeorm.module.ts
@@ -0,0 +1,37 @@
+import { Global, Module } from '@nestjs/common';
+import { DataSource } from 'typeorm';
+
+@Global()
+@Module({
+  imports: [],
+  providers: [
+    {
+      provide: DataSource, // add the datasource as a provider
+      inject: [],
+      useFactory: async () => {
+        // using the factory function to create the datasource instance
+        try {
+          const dataSource = new DataSource({
+            type: 'postgres',
+            host: process.env.DB_HOST,
+            port: parseInt(process.env.DB_PORT),
+            username: process.env.DB_USER,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_NAME,
+            synchronize: false,
+            logging: true,
+            entities: [`${__dirname}/../**/**.entity{.ts,.js}`], // this will automatically load all entity file in the src folder
+          });
+          await dataSource.initialize(); // initialize the data source
+          console.log('Database connected successfully');
+          return dataSource;
+        } catch (error) {
+          console.log('Error connecting to database');
+          throw error;
+        }
+      },
+    },
+  ],
+  exports: [DataSource],
+})
+export class TypeOrmModule {}
